eb0d56b5110a4906186d387a2599a2728f0ebf97
ssskram <paul.marks@pittsburghpa.gov>
2018-05-01 15:36:47 -0400

    scoring algorithm is working fairly well

--- Program.cs
+++ Program.cs
@@ -27,12 +27,12 @@ namespace ASA
             // list to populate with scored names    
             List<scoredName> names = new List<scoredName>();
             
-            Console.WriteLine("Getting all articles");
+            Console.WriteLine("Getting all articles...");
     
             var time = DateTime.UtcNow.AddDays(-30).ToString("s");
             var key = "df76585c7c104053896b14dd3be4d007";
             string endpoint = String.Format
-                ("https://newsapi.org/v2/everything?q='active+shooter' AND (identified OR suspect) NOT (drill OR training)&language=en&pageSize=5&from=2018-04-04&to2018-04-04&apiKey={1}",
+                ("https://newsapi.org/v2/everything?q='active+shooter' AND (identified OR suspect) NOT (drill OR training)&language=en&pageSize=15&from=2018-04-04&to2018-04-04&apiKey={1}",
                     time, // 0
                     key); // 1
             string articles = await client.GetStringAsync(endpoint);
@@ -43,14 +43,12 @@ namespace ASA
 
                 foreach (var item in all)
                 {
-                    // production article ->
-                    string url = item.url.ToString();
-
                     // // ...just for development purposes ->
                     // string url = "http://www.businessinsider.com/youtube-shooting-suspect-name-age-description-headquarters-san-bruno-2018-4";
                     
+                    string url = item.url.ToString();
                     Console.WriteLine(url);
-                    Console.WriteLine("Getting article");
+                    Console.WriteLine("Getting article...");
 
                     string article = null;
                     try
@@ -82,7 +80,7 @@ namespace ASA
                             names.Add(qn);
                         }
                     }
-                    Console.WriteLine("Done! Moving on to the next article...");
+                    Console.WriteLine("Done! Moving on...");
                 }
 
                 await aggregateNames(names);
@@ -100,7 +98,7 @@ namespace ASA
                 // list to populate with possible names
                 List<possibleName> possiblenames = new List<possibleName>();
 
-                Console.WriteLine("...getting possible names");
+                Console.WriteLine("Getting possible names...");
 
                 // get the proper nouns
                 foreach (Match match in n.Matches(article))
@@ -114,11 +112,9 @@ namespace ASA
                             index = match.Index
                         };
                         possiblenames.Add(pona);
-                        
-                        Console.WriteLine("Got one: " + match.Value + " @ position " + match.Index);
                     }
                 }
-                Console.WriteLine("That's all of em.  Sending them back to be scored...");
+                Console.WriteLine("OK, that's all of em.  Sending them back to be scored...");
                 return possiblenames;
             }
 
@@ -135,52 +131,82 @@ namespace ASA
         {
             int score = 0;
 
-            string[] hotwords = new string[] {
+            string[] hot = new string[] {
                 "identified",
                 "Identified",
                 "identify",
                 "Identify",
+                "Confirmed",
+                "confirmed",
                 "suspect",
                 "Suspect",
                 "suspected",
                 "Suspected",
-                "reportedly",
-                "Reportedly",
-                "shooter",
-                "Shooter",
-                "active",
-                "Active",
                 "police",
                 "Police",
                 "wounded",
                 "Wounded",
-                "resident",
-                "Resident",
+                "fatally",
                 "shooting",
                 "Shooting",
-                "Sunday",
+                "shooter",
+                "Shooter",
+                "according",
+                "Victims",
+                "victims"
+            };
+            string[] warm  = new string[] {
+                "reportedly",
+                "Reportedly",
+                "described",
+                "Described",
+                "active",
+                "fire",
+                "Active",
+                "resident",
+                "Resident",
+                "Sunday",   
                 "Monday",
                 "Tuesday",
                 "Wednesday",
                 "Thursday",
                 "Friday",
                 "Saturday",
-                "Sunday",
-                "year",
-                "according",
                 "motive",
                 "investigate",
                 "reported",
                 "Reported",
+                "evidence",
+                "individuals",
+                "targeted",
                 "source",
-                "Sources",
-                "Victims",
-                "victims"
+                "year",
+                "Sources"
+            };
+            string [] cool = new string[] {
+                "have",
+                "that",
+                "the",
+                "in",
+                "the",
+                "was",
+                "a",
+                "who",
+                "of",
+                "as",
+                "knew"
             };
 
             int hwc = 0;
 
-            foreach (string x in hotwords)
+            foreach (string x in hot)
+            {
+                if (name.Contains(x))
+                {
+                    hwc++;
+                }
+            }
+            foreach (string x in warm)
             {
                 if (name.Contains(x))
                 {
@@ -190,11 +216,33 @@ namespace ASA
 
             if (hwc == 0)
             {
-                foreach (string x in hotwords)
+                foreach (string x in hot)
                 {
                     if (article.Contains(x))
                     {
-                        bool check = FindDistance(index, x, article);
+                        bool check = FindDistance(index, x, article, "hot");
+                        if (check == true)
+                        {
+                            score = score + 6;
+                        }
+                    }
+                }
+                foreach (string x in warm)
+                {
+                    if (article.Contains(x))
+                    {
+                        bool check = FindDistance(index, x, article, "warm");
+                        if (check == true)
+                        {
+                            score = score + 2;
+                        }
+                    }
+                }
+                foreach (string x in cool)
+                {
+                    if (article.Contains(x))
+                    {
+                        bool check = FindDistance(index, x, article, "cool");
                         if (check == true)
                         {
                             score = score + 1;
@@ -206,12 +254,26 @@ namespace ASA
             return score;            
         }
 
-        bool FindDistance(int index, string target, string article) 
+        bool FindDistance(int index, string target, string article, string type) 
         {
             bool result = false;
             int before = 0;
             int after = 0;
-            int distance = 1500; // characters
+            int distance;
+
+            if (type == "hot")
+            {
+                distance = 250;
+            }
+            else if (type == "warm")
+            {
+                distance = 400;
+            }
+            else
+            {
+                distance = 500;
+            }
+            
 
             // check target preceding name
             try
@@ -267,7 +329,7 @@ namespace ASA
                 {
                     an.Where(w => w.value == name.value).ToList().ForEach(s => s.score = s.score + name.score);
                 }
-                else
+                else if (name.score != 0)
                 {
                     alreadyEncountered.Add(name.value); 
                     aggregatedName aa = new aggregatedName() 
@@ -287,11 +349,10 @@ namespace ASA
 
             foreach (var name in a)
             {
-                if (name.score > 0)
-                {
-                    Console.WriteLine("Name = " + name.value + "and score  =  " + name.score);
-                }
+                Console.WriteLine("Name = " + name.value + "and score  =  " + name.score);
             }
+
+            // push high scoring names off to scraping function
 
             await Task.Delay(1);
         }
